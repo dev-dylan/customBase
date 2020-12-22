36686c435196c97d15c3b38d3499a2572ef8122f
彭远洋 <pengyuanyang@sensorsdata.cn>
2020-12-22 11:42:30 +0800

    init

---
+++ MDAnalyticsModule/MDAnalyticsModule/MDAnalyticsModule.h
@@ -0,0 +1,17 @@
+//
+//  MDAnalyticsModule.h
+//  MDAnalyticsModule
+//
+//  Created by 彭远洋 on 2020/12/22.
+//
+
+#import <Foundation/Foundation.h>
+#import "WeexSDK.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MDAnalyticsModule : NSObject <WXModuleProtocol>
+
+@end
+
+NS_ASSUME_NONNULL_END
